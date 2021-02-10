9ab4e51daaef53c05bb7899716e6b5d1cbcf30a9
Mark Alves <lamarques.marcos@gmail.com>
2021-02-10 18:22:09 -0300

    New Exercise API training

---
+++ Exercises/api-training/api-index.js
@@ -0,0 +1,6 @@
+
+const myGiphyAPIKey = 'xAFCTFUZzoul10bIZU6JXAzmbMXdk8KJ'
+
+let myGiphyUrl = 'https://api.giphy.com/v1/gifs/translate?api_key=' + myGiphyAPIKey + '&s=cats'
+
+console.log(myGiphyUrl)
