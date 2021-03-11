10a9dcbbed27412b34e153f586b4869dcdc9fd90
Mark Alves <lamarques.marcos@gmail.com>
2021-03-11 15:50:10 -0300

    Started Battleship project

---
+++ battleship-project/src/App.js
@@ -0,0 +1,13 @@
+import Table from './components/Table'
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Table />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ battleship-project/src/components/Table.js
@@ -0,0 +1,373 @@
+import React from 'react'
+import '../styles/Table.css'
+
+
+const Table = () => {
+    return (
+        <table className='battlefield-table'>
+            <tbody>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>1</div>
+                            <div className='marker marker-col'>A</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>B</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>C</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>D</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>E</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>F</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>G</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>H</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>I</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-col'>J</div>
+                        </div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>2</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>3</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>4</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>5</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>6</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>7</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>8</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>9</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+                <tr className='battlefield-row'>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'>
+                            <div className='marker marker-row'>10</div>
+                        </div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                    <td className='battlefield-cell'>
+                        <div className='battlefield-cell-content'></div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
