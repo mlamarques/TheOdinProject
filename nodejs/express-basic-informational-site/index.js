4ec20e58f25221b0670ba9dff982efd5269ea9ef
Mark Alves <lamarques.marcos@gmail.com>
2021-05-29 13:05:00 -0300

    start locallibrary project

--- nodejs/express-basic-informational-site/index.js
+++ nodejs/express-basic-informational-site/index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-
+const exphbs = require('express-handlebars')
+const members = require('./data')
 
 const logger = require('./middleware/logger')
 
 // Init middleware
 // app.use(logger)
 
-//
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 // Body Parser Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}))
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
--- nodejs/express-basic-informational-site/routes/api/members.js
+++ nodejs/express-basic-informational-site/routes/api/members.js
@@ -35,7 +35,8 @@ router.post('/', (req, res) => {
     }
 
     members.push(newMember)
-    res.json(members)
+    // res.json(members)
+    res.redirect('/')
 })
 
 // Update Member
