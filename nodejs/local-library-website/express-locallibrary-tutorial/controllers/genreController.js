cfed923128b7363c36c195efd49e28725bb9ba2f
Mark Alves <lamarques.marcos@gmail.com>
2021-06-28 15:58:57 -0300

    Create get delete genre form

--- nodejs/local-library-website/express-locallibrary-tutorial/controllers/genreController.js
+++ nodejs/local-library-website/express-locallibrary-tutorial/controllers/genreController.js
@@ -98,8 +98,24 @@ exports.genre_create_post = [
   ];
 
 // Display Genre delete form on GET.
-exports.genre_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete GET');
+exports.genre_delete_get = function(req, res, next) {
+  async.parallel({
+    genre: function(callback) {
+        Genre.findById(req.params.id)
+        .exec(callback)
+    },
+    books: function(callback) {
+        Book.find({ 'genre': req.params.id })
+            .exec(callback)
+    },
+  }, function(err, results) {
+      if (err) { return next(err); }
+      if (results.genre==null) { // No results.
+          res.redirect('/catalog/genre');
+      }
+      // Successful, so render.
+      res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, books: results.books } );
+    });
 };
 
 // Handle Genre delete on POST.
