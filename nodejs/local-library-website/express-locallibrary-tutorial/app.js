9eda8666a4591f7668424357823e348faca63255
Mark Alves <lamarques.marcos@gmail.com>
2021-06-30 16:32:43 -0300

    Use a crompression middleware to improve response

--- nodejs/local-library-website/express-locallibrary-tutorial/app.js
+++ nodejs/local-library-website/express-locallibrary-tutorial/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+var compression = require('compression');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
